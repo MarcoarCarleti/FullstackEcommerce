ee06df0b57e5896b25b8a6920343b010d776d7df
MarcoarCarleti <marcarleti@gmai.com>
2025-01-17 15:30:57 -0300

    chore: setup expo router

---
+++ ecommerce-mobile/app/cart.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const CartScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Cart screen</Text>
+    </View>
+  );
+};
+
+export default CartScreen;
---
+++ ecommerce-mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Home screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
