1ecde52f4e91890ab7771a10371bccd34df9d213
MarcoarCarleti <marcarleti@gmai.com>
2025-01-17 16:51:38 -0300

    feat: product details screen

--- ecommerce-mobile/components/product-list-item.tsx
+++ ecommerce-mobile/components/product-list-item.tsx
@@ -3,46 +3,32 @@ import React from "react";
 import { Card } from "./ui/card";
 import { Image } from "./ui/image";
 import { Text } from "./ui/text";
-import { VStack } from "./ui/vstack";
 import { Heading } from "./ui/heading";
-import { Box } from "./ui/box";
-import { Button, ButtonText } from "./ui/button";
 import { formatCurrency } from "@/utils";
+import { Pressable } from "react-native";
 
 function ProductListItem({ product }) {
   return (
-    <Card className="p-5 rounded-lg max-w-[360px] flex-1">
-      <Image
-        source={{
-          uri: product.image,
-        }}
-        className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
-        alt={`${product.name} image`}
-        resizeMode="contain"
-      />
-      <Text className="text-sm font-normal mb-2 text-typography-700">
-        {product.name}
-      </Text>
-      <VStack className="mb-6">
-        <Heading size="md" className="mb-4">
-          {formatCurrency(product.price)}
-        </Heading>
-        <Text size="sm">{product.description}</Text>
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Carrinho</ButtonText>
-        </Button>
-        <Button
-          variant="outline"
-          className="px-4 py-2 border-outline-300 sm:flex-1"
-        >
-          <ButtonText size="sm" className="text-typography-600">
-            Lista de desejos
-          </ButtonText>
-        </Button>
-      </Box>
-    </Card>
+    <Link href={`product/${product.id}`} asChild>
+      <Pressable className="flex-1">
+        <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+          <Image
+            source={{
+              uri: product.image,
+            }}
+            className="mb-6 h-[240px] w-full rounded-md"
+            alt={`${product.name} image`}
+            resizeMode="contain"
+          />
+          <Text className="text-sm font-normal mb-2 text-typography-700">
+            {product.name}
+          </Text>
+          <Heading size="md" className="mb-4">
+            {formatCurrency(product.price)}
+          </Heading>
+        </Card>
+      </Pressable>
+    </Link>
   );
 }
 
