a7a4e46805884996a17dc3437318a40eb71a4c4a
MarcoarCarleti <marcarleti@gmai.com>
2025-01-18 11:29:47 -0300

    feat: shopping cart with global state using zustand

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,19 +1,50 @@
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Icon } from "@/components/ui/icon";
+import { ShoppingCart } from "lucide-react-native";
+import { Pressable } from "react-native";
+import { Text } from "@/components/ui/text";
+import { useCart } from "@/store/cart-store";
 
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
+  const cartItemsNum = useCart((state) => state.items.length);
+
   return (
     <QueryClientProvider client={queryClient}>
       <GluestackUIProvider>
-        <Stack>
+        <Stack
+          screenOptions={{
+            headerRight: () =>
+              cartItemsNum > 0 && (
+                <Link href={"/cart"} asChild>
+                  <Pressable className="flex-row gap-2">
+                    <Icon as={ShoppingCart} />
+                    <Text>{cartItemsNum}</Text>
+                  </Pressable>
+                </Link>
+              ),
+          }}
+        >
           <Stack.Screen
             name="index"
             options={{
-              title: "Shopping",
+              title: "Produtos",
+            }}
+          />
+          <Stack.Screen
+            name="product/[id]"
+            options={{
+              title: "Produto",
+            }}
+          />
+          <Stack.Screen
+            name="cart"
+            options={{
+              title: "Carrinho",
             }}
           />
         </Stack>
--- ecommerce-mobile/app/cart.tsx
+++ ecommerce-mobile/app/cart.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { useCart } from "@/store/cart-store";
+import { formatCurrency } from "@/utils";
+import { Redirect } from "expo-router";
+import { View, FlatList } from "react-native";
 
 const CartScreen = () => {
+  const items = useCart((state) => state.items);
+  const resetCart = useCart((state) => state.resetCart);
+
+  async function onCheckout() {
+    resetCart();
+  }
+
+  if (items.length <= 0) {
+    return <Redirect href={"/"} />;
+  }
+
   return (
-    <View>
-      <Text style={{ fontSize: 30 }}>Cart screen</Text>
-    </View>
+    <FlatList
+      data={items}
+      contentContainerClassName="gap-2 max-w-[960px] w-full mx-auto p-2"
+      renderItem={({ item }) => (
+        <Box>
+          <HStack className="bg-white p-3">
+            <VStack space="sm">
+              <Text bold>{item.product.name}</Text>
+              <Text>{formatCurrency(item.product.price)}</Text>
+            </VStack>
+            <Text className="ml-auto">{item.quantity}</Text>
+          </HStack>
+        </Box>
+      )}
+      ListFooterComponent={() => (
+        <Button onPress={onCheckout}>
+          <ButtonText>Checkout</ButtonText>
+        </Button>
+      )}
+    />
   );
 };
 
